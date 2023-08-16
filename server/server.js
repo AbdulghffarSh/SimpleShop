df25bf45f637094acc3140a85e967e08d012de44
Abdulgaffar <Abdulgaffar.sh@outlook.com>
2023-08-16 11:32:13 +0200

    final commit

--- server/server.js
+++ server/server.js
@@ -1,30 +1,26 @@
 require("dotenv").config();
 
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
-app.use(
-  cors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:3000"],
-  })
-);
+const app = express();
 
+app.use(cors({
+  origin: ["http://localhost:5500", "http://127.0.0.1:3000"],
+}));
 app.use(express.static("public"));
 app.use(express.json());
 
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
-
 const storeItems = new Map([
-  [1, { priceInCents: 1000, name: "Learn React Today" }],
-  [2, { priceInCents: 2000, name: "Learn CSS Today" }],
-])
+  [1, { priceInCents: 1000, name: "T-Shirt" }],
+  [2, { priceInCents: 2000, name: "Shoes" }],
+]);
 
 app.get("/items", (req, res) => {
-  const itemsArray = Array.from(storeItems); 
+  const itemsArray = Array.from(storeItems);
   res.json(itemsArray);
 });
-
 
 app.post("/create-checkout-session", async (req, res) => {
   try {
@@ -32,7 +28,7 @@ app.post("/create-checkout-session", async (req, res) => {
       payment_method_types: ["card"],
       mode: "payment",
       line_items: req.body.items.map(item => {
-        const storeItem = storeItems.get(item.id)
+        const storeItem = storeItems.get(item.id);
         return {
           price_data: {
             currency: "eur",
@@ -42,7 +38,7 @@ app.post("/create-checkout-session", async (req, res) => {
             unit_amount: storeItem.priceInCents,
           },
           quantity: item.quantity,
-        }
+        };
       }),
       success_url: `${process.env.CLIENT_URL}/success.html`,
       cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
@@ -51,6 +47,35 @@ app.post("/create-checkout-session", async (req, res) => {
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
+});
+
+app.post('/send_email', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'your_email_service', // Example: 'Gmail'
+    auth: {
+      user: 'your_email@example.com',
+      pass: 'your_email_password',
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: 'simpleShop@test.com', // Change this to the recipient's email
+    subject: 'Contact Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error sending message.');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Message sent successfully!');
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3000;
